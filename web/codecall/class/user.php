3ef3d4200561de8daad03c8c0b0399d0344c63f7
Emmanuel Soriano <emmanuelsoriano@openmailbox.org>
2016-03-02 11:52:50 -0800

    Fixed registration. Redirection to login page

--- web/codecall/class/user.php
+++ web/codecall/class/user.php
@@ -118,7 +118,7 @@
 				
 				$stmt->execute();
 				echo "Successfully Recorded ";
-				header("location:/thesis/web/index.html");	
+				header("location:/thesis/web/codecall/index.php");	
 			}catch( PDOException $e ) {
 				return $e->getMessage();
 			}
