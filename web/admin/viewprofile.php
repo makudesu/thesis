f7f6172ec7a75dfe0a8e1b829875d8c9158d1f2d
makudesu <makudesu@grex.org>
2016-03-20 21:29:42 +0800

    Added bnhs-admin.herokuapp.com as iframe in admin

--- web/admin/viewprofile.php
+++ web/admin/viewprofile.php
@@ -92,7 +92,7 @@
     <section id="content-wrapper">
 
 
-<iframe width="100%" height="100%" scrolling="no" name="iframe1" src="table.php"></iframe>
+<iframe width="100%" height="100%" scrolling="no" name="iframe1" src="https://bnhs-admin.herokuapp.com"></iframe>
     </section><!-- content-wrapper end -->
 
     <!-- footer start -->
